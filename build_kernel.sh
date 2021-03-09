e18fe1cc1a8986ffb26b29e5eb3e78f693a3f3ff
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-09 21:53:56 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir out
+wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+unzip gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+export CROSS_COMPILE=${PWD}/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 #rm log.txt
 #make clean
 #make distclean
 #export CROSS_COMPILE=/home/tien.tq1/toolchains/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 
-make exynos7580-a5xelte_defconfig
+make O=out exynos7580-j7elte_defconfig
+#make -j$(nproc) O=out 2>&1 | tee kernel.log
 make -j
 #24 2>&1 | tee -a  log.txt
