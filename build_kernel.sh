599980a5a65e35f0ba8298a9f3a31fab22ba4625
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 16:32:03 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir out
+sudo apt-get install gcc-arm-linux-gnueabi 
 wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 unzip gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 export CROSS_COMPILE=${PWD}/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
