05f9df12a881766f97bd96280fa7aa9fb32901cc
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 17:10:52 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,13 +3,14 @@ mkdir out
 sudo apt-get install gcc-arm-linux-gnueabi && sudo apt install xz-utils
 wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 tar xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+echo unzip complete
 export CROSS_COMPILE=${PWD}/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 #rm log.txt
 #make clean
 #make distclean
 #export CROSS_COMPILE=/home/tien.tq1/toolchains/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
-#export SUBARCH=arm64
+export SUBARCH=arm64
 
 make O=out lineageos_j7elte_defconfig
 make -j$(nproc) O=out 2>&1 | tee kernel.log
