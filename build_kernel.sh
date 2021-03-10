9245c3aa0058fa6b08b4299ccd3b045c45d3dca1
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 16:45:11 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir out
-sudo apt-get install gcc-arm-linux-gnueabi 
+sudo apt-get install gcc-arm-linux-gnueabi && sudo apt install xz-utils
 wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
-unzip gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+tar xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 export CROSS_COMPILE=${PWD}/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 #rm log.txt
 #make clean
@@ -12,6 +12,6 @@ export ARCH=arm64
 export SUBARCH=arm64
 
 make exynos7580-j7elte_defconfig
-make -j$(nproc) O=out 2>&1 | tee kernel.log
-#make -j
+#make -j$(nproc) O=out 2>&1 | tee kernel.log
+make -j
 #24 2>&1 | tee -a  log.txt
