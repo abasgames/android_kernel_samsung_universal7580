0b088726a176a192c1b682da9053dd69ee913e50
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 23:27:54 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ wget http://releases.linaro.org/components/toolchain/gcc-linaro/4.9-2017.01/gcc-
 tar xf gcc-linaro-4.9-2017.01.tar.xz
 echo unzip complete
 export CROSS_COMPILE=${PWD}/gcc-linaro-4.9-2017.01/
-chmod 755 /gcc-linaro-4.9-2017.01/gcc
+sudo chmod 755 /gcc-linaro-4.9-2017.01/gcc
 #rm log.txt
 #echo wtf
 #make clean
@@ -14,4 +14,5 @@ export ARCH=arm64
 export SUBARCH=arm64
 
 make O=out lineageos_j7elte_defconfig
+su
 make -j$(nproc) O=out 2>&1 | tee kernel.log
