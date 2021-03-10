f214ecfcd7861b953048b17705e44148fa4f100f
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 23:20:46 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,6 +4,7 @@ wget http://releases.linaro.org/components/toolchain/gcc-linaro/4.9-2017.01/gcc-
 tar xf gcc-linaro-4.9-2017.01.tar.xz
 echo unzip complete
 export CROSS_COMPILE=${PWD}/gcc-linaro-4.9-2017.01/
+chmod 755 /gcc-linaro-4.9-2017.01/gcc
 #rm log.txt
 #make clean
 #make distclean
