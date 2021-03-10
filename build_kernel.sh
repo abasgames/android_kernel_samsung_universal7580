afc3a4889674481ce85dd23c61a5f923fe3b4d93
abasgames <45899491+abasgames@users.noreply.github.com>
2021-03-10 20:46:33 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 mkdir out
+cd
 sudo apt-get install gcc-arm-linux-gnueabi && sudo apt install xz-utils
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-#wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
-#tar xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+#git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
+wget http://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-linux-gnu/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
+tar xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu.tar.xz
 echo cloning gcc complete
-#export CROSS_COMPILE=${PWD}/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=${PWD}~/android_kernel_samsung_universal7580/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
 #rm log.txt
 #make clean
 #make distclean
-export CROSS_COMPILE=~/android_kernel_samsung_universal7580/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+#export CROSS_COMPILE=~/android_kernel_samsung_universal7580/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export ARCH=arm64
-#export SUBARCH=arm64
+export SUBARCH=arm64
 
-make lineageos_j7elte_defconfig
-#make -j$(nproc) O=out 2>&1 | tee kernel.log
-make -j
+make O=out lineageos_j7elte_defconfig
+make -j$(nproc) O=out 2>&1 | tee kernel.log
+#make -j
 #24 2>&1 | tee -a  log.txt
